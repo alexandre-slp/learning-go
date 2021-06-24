156a154479f9edd5a7c2de101347fc185e38dc66
Alexandre Paes <alexandre_slp@poli.ufrj.br>
2021-06-24 12:48:09 -0300

    just making code more clear

--- jedi_level_10/hands_on_exercise_7_b.go
+++ jedi_level_10/hands_on_exercise_7_b.go
@@ -4,13 +4,14 @@ import "fmt"
 
 func main() {
 	numChan := make(chan int)
+	totalGoroutines := 10
 
-	for i := 0; i < 10; i++ {
+	for i := 0; i < totalGoroutines; i++ {
 		go func(numGor int) {
 			for j := 0; j < 10; j++ {
 				numChan <- j
 			}
-			if numGor == 9 {
+			if numGor == totalGoroutines-1 {
 				close(numChan)
 			}
 		}(i)
