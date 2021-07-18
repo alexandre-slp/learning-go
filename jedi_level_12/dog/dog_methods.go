96dfd532b96d1d084574ae1fd154d928415d168a
Alexandre Paes <alexandre_slp@poli.ufrj.br>
2021-07-18 02:00:48 -0300

    added jedi level 12

---
+++ jedi_level_12/dog/dog_methods.go
@@ -0,0 +1,7 @@
+// Package dog description
+package dog
+
+// Years convert human years to dog years.
+func Years(humanYears int) int {
+	return 7 * humanYears
+}
---
+++ jedi_level_12/hands_on_exercise_1.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/alexandre-slp/learning-go/jedi_level_12/dog"
+)
+
+func main() {
+	dy := dog.Years(5)
+	fmt.Println(dy)
+}
